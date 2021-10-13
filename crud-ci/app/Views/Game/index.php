d17e8857e78e29ae1dd6278005b78d2f1b1b3eac
Asril Rinaldi <asrilrinaldi@gmail.com>
2021-10-13 22:10:28 +0700

    add modal edit

--- crud-ci/app/Views/Game/index.php
+++ crud-ci/app/Views/Game/index.php
@@ -52,10 +52,10 @@
                             <td scope="row"><?= $i; ?></td>
                             <td><?= $row['nama']; ?></td>
                             <td><?= $row['genre']; ?></td>
-                            <td><?= $row['ukuran']; ?></td>
+                            <td><?= $row['ukuran']; ?> MB</td>
                             <td><?= $row['tahun']; ?></td>
                             <td>
-                                <button class="btn btn-warning btn-sm"><i class="fa fa-edit"></i></button>
+                                <button type="button" data-toggle="modal" data-target="#modalEdit" id="btn-edit" class="btn btn-sm btn-warning" data-id="<?= $row['id'] ?>" data-nama="<?= $row['nama'] ?>" data-genre="<?= $row['genre'] ?>" data-ukuran="<?= $row['ukuran'] ?>" data-tahun="<?= $row['tahun'] ?>"> <i class="fa fa-edit"></i> </button>
                                 <button type="button" data-toggle="modal" data-target="#modalHapus" id="btn-hapus" class="btn btn-sm btn-danger" data-id="<?= $row['id']; ?>"> <i class="fa fa-trash-alt"></i> </button>
                             </td>
 
@@ -76,7 +76,7 @@
 </div>
 <!-- End of Main Content -->
 
-<!-- Modal -->
+<!-- Modal Tambah -->
 <div class="modal fade" id="modalTambah">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
@@ -128,6 +128,62 @@
         </div>
     </div>
 </div>
+
+
+<!-- Modal Edit -->
+<div class="modal fade" id="modalEdit">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5>Edit Data</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form action="<?= base_url('game/edit'); ?>" method="POST">
+                    <input type="hidden" name="id" id="id-game">
+                    <div class="form-group mb-0">
+                        <label for="nama">Nama</label><br>
+                        <input type="text" name="nama" id="nama" class="form-control" placeholder="Masukkan Nama" required="" oninvalid="this.setCustomValidity('Masukkan Nama')" oninput="this.setCustomValidity('')" value="<?= $row['nama'] ?>">
+                    </div><br>
+                    <div class="form-group mb-0">
+                        <label for="genre">Genre</label><br>
+                        <select name="genre" id="genre" class="form-control" required="" oninvalid="this.setCustomValidity('Pilih Genre')" oninput="this.setCustomValidity('')">
+                            <option value="">-Pilih Genre-</option>
+                            <option value="Actions">Actions</option>
+                            <option value="Moba">Moba</option>
+                            <option value="Puzzle">Puzzle</option>
+                        </select>
+                    </div><br>
+                    <div class="form-group mb-0">
+                        <label for="ukuran">Ukuran</label><br>
+                        <input type="text" name="ukuran" id="ukuran" class="form-control" placeholder="Ukuran Game" required="" oninvalid="this.setCustomValidity('Ukuran Game')" oninput="this.setCustomValidity('')">
+                    </div><br>
+                    <div class="form-group mb-0">
+                        <label for="tahun">Tahun</label><br>
+                        <select name="tahun" id="tahun" class="form-control" required="" oninvalid="this.setCustomValidity('Pilih Tahun')" oninput="this.setCustomValidity('')">
+                            <option value="">-Pilih Tahun-</option>
+                            <option value="2000">2000</option>
+                            <option value="2001">2001</option>
+                            <option value="2002">2002</option>
+                            <option value="2003">2003</option>
+                            <option value="2004">2004</option>
+                            <option value="2005">2005</option>
+                            <option value="2006">2006</option>
+                        </select>
+                    </div><br>
+
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Batal</button>
+                <button type="submit" name="edit" class="btn btn-primary">Simpan</button>
+            </div>
+            </form>
+        </div>
+    </div>
+</div>
+
 
 
 <!-- Modal Hapus  -->
