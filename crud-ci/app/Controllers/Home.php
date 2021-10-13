772d90999e1096d266fbc99501498636e2e36c94
Asril Rinaldi <asrilrinaldi@gmail.com>
2021-10-13 13:16:28 +0700

    add admin bootstrap

--- crud-ci/app/Controllers/Home.php
+++ crud-ci/app/Controllers/Home.php
@@ -6,6 +6,10 @@ class Home extends BaseController
 {
     public function index()
     {
-        return view('welcome_message');
+        echo view('templates/v_header');
+        echo view('templates/v_sidebar');
+        echo view('templates/v_topbar');
+        echo view('home/index.php');
+        echo view('templates/v_footer');
     }
 }
