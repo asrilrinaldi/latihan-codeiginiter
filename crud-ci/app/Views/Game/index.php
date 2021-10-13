7ab2cfda2350c747dcbf58972285106a62e5ab15
Asril Rinaldi <asrilrinaldi@gmail.com>
2021-10-13 20:42:22 +0700

    modal hapus

--- crud-ci/app/Views/Game/index.php
+++ crud-ci/app/Views/Game/index.php
@@ -1,4 +1,3 @@
-<!-- Begin Page Content -->
 <div class="container-fluid">
 
     <!-- Page Heading -->
@@ -15,6 +14,16 @@
             $("alert").alert();
         </script>
     <?php endif;  ?>
+
+    <div class="row">
+        <div class="col-md-6">
+            <?php
+            if (session()->get('err')) {
+                echo " <div class='alert alert-danger' role='alert'>" . session()->get('err') . " </div>";
+            }
+            ?>
+        </div>
+    </div>
 
     <div class="card">
         <div class="card-header">
@@ -47,7 +56,7 @@
                             <td><?= $row['tahun']; ?></td>
                             <td>
                                 <button class="btn btn-warning btn-sm"><i class="fa fa-edit"></i></button>
-                                <button class="btn btn-danger btn-sm"><i class="fa fa-trash"></i></button>
+                                <button type="button" data-toggle="modal" data-target="#modalHapus" id="btn-hapus" class="btn btn-sm btn-danger" data-id="<?= $row['id']; ?>"> <i class="fa fa-trash-alt"></i> </button>
                             </td>
 
 
@@ -81,11 +90,11 @@
                 <form action="<?= base_url('game/tambah'); ?>" method="POST">
                     <div class="form-group mb-0">
                         <label for="nama">Nama</label><br>
-                        <input type="text" name="nama" id="nama" class="form-control" placeholder="Masukkan Nama">
+                        <input type="text" name="nama" id="nama" class="form-control" placeholder="Masukkan Nama" required="" oninvalid="this.setCustomValidity('Masukkan Nama')" oninput="this.setCustomValidity('')">
                     </div><br>
                     <div class="form-group mb-0">
                         <label for="genre">Genre</label><br>
-                        <select name="genre" id="genre" class="form-control">
+                        <select name="genre" id="genre" class="form-control" required="" oninvalid="this.setCustomValidity('Pilih Genre')" oninput="this.setCustomValidity('')">
                             <option value="">-Pilih Genre-</option>
                             <option value="Actions">Actions</option>
                             <option value="Moba">Moba</option>
@@ -94,11 +103,11 @@
                     </div><br>
                     <div class="form-group mb-0">
                         <label for="ukuran">Ukuran</label><br>
-                        <input type="text" name="ukuran" id="ukuran" class="form-control" placeholder="Ukuran Game">
+                        <input type="text" name="ukuran" id="ukuran" class="form-control" placeholder="Ukuran Game" required="" oninvalid="this.setCustomValidity('Ukuran Game')" oninput="this.setCustomValidity('')">
                     </div><br>
                     <div class="form-group mb-0">
                         <label for="tahun">Tahun</label><br>
-                        <select name="tahun" id="tahun" class="form-control">
+                        <select name="tahun" id="tahun" class="form-control" required="" oninvalid="this.setCustomValidity('Pilih Tahun')" oninput="this.setCustomValidity('')">
                             <option value="">-Pilih Tahun-</option>
                             <option value="2000">2000</option>
                             <option value="2001">2001</option>
@@ -113,8 +122,27 @@
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-dismiss="modal">Batal</button>
-                <button type="submit" class="btn btn-primary">Simpan</button>
+                <button type="submit" name="tambah" class="btn btn-primary">Simpan</button>
             </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+
+<!-- Modal Hapus  -->
+<div class="modal fade" id="modalHapus">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <form action="/game/hapus" method="post">
+                <div class="modal-body">
+                    Apakah anda yakin ingin menghapus data ini?
+                    <input type="hidden" id="id" name="id">
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-primary">Yakin</button>
+                </div>
             </form>
         </div>
     </div>
